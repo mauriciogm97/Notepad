ec42291e66c7b81214f34c2d1b7b505f413e2c85
mauriciogm97 <mauriciogm97@users.noreply.github.com>
2019-11-30 15:02:25 -0600

    Added CORS

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = require('./routes.js')
+var cors = require('cors');
 require('./db/db.js')
 
 const port = process.env.PORT || 3000
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
